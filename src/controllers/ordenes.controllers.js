7bdd1a319d93f81d40b787016c3669b33fa81856
Gustavo Wirzt <gwirzt@hotmail.com>
2023-02-01 12:11:37 -0300

    Cambie los codigod e proveedor por cuit en la busqueda de ordenes

--- src/controllers/ordenes.controllers.js
+++ src/controllers/ordenes.controllers.js
@@ -5,10 +5,13 @@ const ordenesController = {}
 ordenesController.getOrdenesProveedorTodas = async (req, res) => {
     
     const pool = await getConnection();
-    const result = await pool.request().query("SELECT  ctrl as OC, fecha, moneda, importe, "+
-    "case when cerrada = 1 then 'Cerrada' else 'Abierta' end as estado "+
-    "  FROM p_ordcompra where provedor = " + req.body.provedor + 
-    "  order by 5 , 1  ");
+    const result = await pool.request().query("SELECT  a.ctrl as OC, a.fecha, "+
+    "case when a.moneda is null then 'P' else a.moneda end as moneda, a.importe, "+
+    " case when cerrada = 1 then 'Cerrada' else 'Abierta' end as estado "+
+    " FROM p_ordcompra a "+
+    " left join m_provedores b on a.provedor = b.ctrl "+
+    " where b.cuit = '" + req.body.cuit + "'"+ 
+    " order by 5 , 1  ");
     res.json(result.recordset)
 };
 
@@ -18,22 +21,28 @@ ordenesController.getOrdenesProveedorTodas = async (req, res) => {
 ordenesController.getOrdenesProveedorAbiertas = async (req, res) => {
     
     const pool = await getConnection();
-    const result = await pool.request().query("SELECT  ctrl as OC, fecha, moneda, importe, "+
-    "case when cerrada = 1 then 'Cerrada' else 'Abierta' end as estado "+
-    "  FROM p_ordcompra where provedor = " + req.body.provedor + 
-    " and  cerrada is null"+
-    "  order by 5 , 1  ");
+    const result = await pool.request().query("SELECT  a.ctrl as OC, a.fecha, "+
+    " case when a.moneda is null then 'P' else a.moneda end as moneda, a.importe, "+
+    " case when a.cerrada = 1 then 'Cerrada' else 'Abierta' end as estado "+
+    " FROM p_ordcompra a "+
+    " left join m_provedores b on a.provedor = b.ctrl "+
+    " where b.cuit = '" + req.body.cuit + "'"+
+    " and  a.cerrada is null"+
+    " order by 5 , 1  ");
     res.json(result.recordset)
 };
 
 ordenesController.getOrdenesProveedorCerradas = async (req, res) => {
     
     const pool = await getConnection();
-    const result = await pool.request().query("SELECT  ctrl as OC, fecha, moneda, importe, "+
-    "case when cerrada = 1 then 'Cerrada' else 'Abierta' end as estado "+
-    "  FROM p_ordcompra where provedor = " + req.body.provedor + 
-    " and cerrada = 1"+
-    "  order by 5 , 1  ");
+    const result = await pool.request().query("SELECT  a.ctrl as OC, a.fecha, "+
+    " case when a.moneda is null then 'P' else a.moneda end as moneda, a.importe, "+
+    " case when a.cerrada = 1 then 'Cerrada' else 'Abierta' end as estado "+
+    " FROM p_ordcompra a "+
+    " left join m_provedores b on a.provedor = b.ctrl "+
+    " where b.cuit = '" + req.body.cuit + "'"+
+    " and a.cerrada = 1"+
+    " order by 5 , 1  ");
     res.json(result.recordset)
 };
 
